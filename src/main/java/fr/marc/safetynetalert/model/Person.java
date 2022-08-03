49976d1f85ee46d43a1a19ff3edd14b8a76cee72
Marc MASSE <marc.masse600@orange.fr>
2022-08-03 11:09:01 +0200

    PersonService class created

--- src/main/java/fr/marc/safetynetalert/model/Person.java
+++ src/main/java/fr/marc/safetynetalert/model/Person.java
@@ -9,29 +9,35 @@ import javax.persistence.Table;
 
 import lombok.Data;
 
+/*
+ * This class represents the "Persons" table of the database
+ * 
+ * @Author Marc
+ */
+
 @Data
 @Entity
 @Table(name = "persons")
 public class Person {
-	
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    @Column(name="first_name")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "first_name")
 	private String firstName;
 
-    @Column(name="last_name")
+	@Column(name = "last_name")
 	private String lastName;
-	
+
 	private String address;
-	
+
 	private String city;
-	
+
 	private int zip;
-	
+
 	private String phone;
-	
+
 	private String email;
-	
+
 }
--- src/main/java/fr/marc/safetynetalert/repository/PersonRepository.java
+++ src/main/java/fr/marc/safetynetalert/repository/PersonRepository.java
@@ -5,6 +5,12 @@ import org.springframework.stereotype.Repository;
 
 import fr.marc.safetynetalert.model.Person;
 
+/*
+ * Spring Data JPA interface to execute SQL queries, without needing to write them.
+ * 
+ * @Author Marc
+ */
+
 @Repository
 public interface PersonRepository extends CrudRepository<Person, Long> {
 
